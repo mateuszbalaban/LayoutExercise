896d1fc3d3991c0465f2cc53ea1cf3f94720bc51
mateuszbalaban <mateusz.balaban@icloud.com>
2024-10-08 18:13:19 +0200

    Initial Commit

---
+++ MyLayoutTests/MyLayoutTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyLayoutTests.swift
+//  MyLayoutTests
+//
+//  Created by Mateusz Bałaban on 08/10/2024.
+//
+
+import Testing
+@testable import MyLayout
+
+struct MyLayoutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
